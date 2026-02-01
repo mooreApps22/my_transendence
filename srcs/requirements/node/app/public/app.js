8e8a8b50dab54e8c6d393e1e4655c6b19bed40fa
Skyy Moore <mooreapps2022@gmail.com>
2026-02-01 12:51:40 -0800

    String interpolation

--- srcs/requirements/node/app/public/app.js
+++ srcs/requirements/node/app/public/app.js
@@ -1,3 +1,18 @@
 document.getElementById("myH1").textContent = "Hi Pimp";
-document.getElementById("myP1").textContent = "It's hard out here for a pimp!";
-document.getElementById("myP2").textContent = "When you trying to make the money for the rent.";
+
+let x = "It's hard out here for a pimp!";
+let y = "When you trying to make the money for the rent.";
+
+document.getElementById("myP1").textContent = x;
+document.getElementById("myP2").textContent = y;
+
+let age = 25;
+let msg = `The average age is ${age}.`
+
+console.log(msg);
+console.log(typeof age);
+
+let firstName = "Skyy";
+let lastName = "Moore";
+
+console.log(`My name is ${firstName} ${lastName}`)
